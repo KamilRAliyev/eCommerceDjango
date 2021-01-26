e9f802437ffa7a9e8deffaa775e3767d0974ea73
KamilRAliyev <kamiraliyev@gmail.com>
2021-01-26 18:27:31 +0400

    Templates are ready.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,4 +3,10 @@ from  django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("HomePage")
+    return render(request, 'accounts/content/dashboard.html')
+
+def products(request):
+    return render(request, 'accounts/content/products.html')
+
+def customer(request):
+    return render(request, 'accounts/content/customer.html')
